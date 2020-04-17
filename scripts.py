cc33b9e8ba61adfdde395dd5ec68c05438320976
ksg97031 <ksg97031@naver.com>
2020-04-18 00:22:28 +0900

    Add aquatone, subfinder

--- scripts.py
+++ scripts.py
@@ -7,7 +7,7 @@ def badget_md(category, target):
 
 if __name__ == '__main__':
     md = '| Name | URL | Description | Preview | Popularity | Metadata |'
-    md += '\n| ---------- | :---------- | :---------- | :----------- | :---------: | :----------: |'
+    md += '\n| ---------- | :---------- | :---------- | :-----------: | :---------: | :----------: |'
     preview_md = "\n"
     with open('list.txt') as f:
         lines = f.read().strip().split("\n")
@@ -25,7 +25,7 @@ if __name__ == '__main__':
             target = github_url.split('github.com/')[1]
             popul = badget_md('stars', target)
             badges = list(map(lambda x : badget_md(x, target), categories))
-            preview_link = "[{0}](#{0})".format(name)
+            preview_link = "[Watch](#{0})".format(name)
             preview_md += "## {name}  \n[![asciicast]({preview_video_url}.svg)]({preview_video_url})\n".format(name=name,preview_video_url=preview_video_url)
                 
             md += '\n| **{name}** | [{github_url}]({github_url}) | {desc} | {preview_link} | {popul} | {badges} |'.format(desc=desc, github_url=github_url, name=name, preview_link=preview_link, popul=popul, badges=' '.join(badges)) 
